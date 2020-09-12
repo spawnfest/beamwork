211e1b533e10e0e5a811bbfc0725c9ffd87073d8
Derek Kraan <derek.kraan@gmail.com>
2020-09-12 09:38:23 +0200

    Add dog_sketch

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Spotlight.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:dog_sketch, "~> 0.1.0"}
     ]
   end
 
